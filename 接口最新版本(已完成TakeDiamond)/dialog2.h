1defbbdffc5548fe59da4c107e2a8694bc19ad31
WinghoLeung <82169779+WinghoLeung@users.noreply.github.com>
2021-04-23 07:49:05 -0500

    Add files via upload

---
+++ 接口最新版本(已完成TakeDiamond)/dialog2.h
@@ -0,0 +1,53 @@
+#ifndef DIALOG2_H
+#define DIALOG2_H
+#include "Splendor.h"
+#include <QDialog>
+#include <QCloseEvent>
+#include <QToolButton>
+
+namespace Ui {
+class Dialog2;
+}
+
+class Dialog2 : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog2(QWidget *parent = 0);
+    ~Dialog2();
+    void selectDiamond(color);//记录所选宝石的信息
+    void clearDiamond();//清空临时变量
+    bool getStatus()const{
+        return status;
+    }
+    void OpenStatus(){
+        status = 1;
+    }
+private:
+    Ui::Dialog2 *ui;
+    color selected[3]={Gold,Gold,Gold};//储存已选择宝石的颜色,Gold表示空
+    bool status = 0;//记录窗口是否已打开,0为关闭,1为打开
+
+    void showDiamond(QToolButton *,color);//显示颜色,内部调用
+
+signals:
+    void dialog2OK();
+    void dialog2close();
+    void Cancel_selected(color);
+
+protected slots:
+    void select1_clicked();
+    void ClickedOK()
+    {
+        emit dialog2OK();
+    }
+protected:
+    void closeEvent(QCloseEvent *) override;
+private slots:
+    void on_selected1_clicked();
+    void on_selected2_clicked();
+    void on_selected3_clicked();
+};
+
+#endif // DIALOG2_H
---
+++ 接口最新版本(已完成TakeDiamond)/ui_dialog.h
@@ -0,0 +1,70 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOG_H
+#define UI_DIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QToolButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog
+{
+public:
+    QLabel *label;
+    QToolButton *purchase;
+    QToolButton *cancel;
+
+    void setupUi(QDialog *Dialog)
+    {
+        if (Dialog->objectName().isEmpty())
+            Dialog->setObjectName(QStringLiteral("Dialog"));
+        Dialog->resize(342, 383);
+        Dialog->setMinimumSize(QSize(342, 383));
+        Dialog->setMaximumSize(QSize(342, 383));
+        Dialog->setStyleSheet(QStringLiteral(""));
+        label = new QLabel(Dialog);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(60, 10, 201, 281));
+        purchase = new QToolButton(Dialog);
+        purchase->setObjectName(QStringLiteral("purchase"));
+        purchase->setGeometry(QRect(40, 320, 101, 51));
+        purchase->setStyleSheet(QStringLiteral(""));
+        cancel = new QToolButton(Dialog);
+        cancel->setObjectName(QStringLiteral("cancel"));
+        cancel->setGeometry(QRect(190, 320, 111, 51));
+
+        retranslateUi(Dialog);
+
+        QMetaObject::connectSlotsByName(Dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog)
+    {
+        Dialog->setWindowTitle(QApplication::translate("Dialog", "Dialog", Q_NULLPTR));
+        label->setText(QString());
+        purchase->setText(QApplication::translate("Dialog", "Purchase", Q_NULLPTR));
+        cancel->setText(QApplication::translate("Dialog", "Cancel", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog: public Ui_Dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOG_H
