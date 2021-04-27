14ce522972be48c939956c3b1b93497a5548ee91
WinghoLeung <82169779+WinghoLeung@users.noreply.github.com>
2021-04-26 21:08:29 -0500

    Add files via upload
    前端后端的所有文件都有更改，可以从这个版本开始改，但跑不起来
    值得一提的是，图片文件不要有中文，QRC文件会读不了路径

---
+++ 接口最新版本(合并前端后端)/interface/gameinterface.h
@@ -0,0 +1,107 @@
+#ifndef GAMEINTERFACE_H
+#define GAMEINTERFACE_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QGridLayout>
+#include "dialog.h"
+#include "dialog2.h"
+#include "Splendor.h"
+
+namespace Ui {
+class gameInterface;
+}
+
+class gameInterface : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit gameInterface(QWidget *parent = 0);
+    ~gameInterface();
+
+private slots:
+    void on_card1_1_clicked();
+
+    void on_people1_clicked();
+
+    void on_people2_clicked();
+
+    void on_require1_1_1_textChanged();
+
+    void on_require1_1_3_textChanged();
+
+    void on_card1_2_clicked();
+
+    void on_card1_3_clicked();
+
+    void on_card1_4_clicked();
+
+    void on_card2_1_clicked();
+
+    void on_card2_2_clicked();
+
+    void on_card2_3_clicked();
+
+    void on_card2_4_clicked();
+
+    void on_card3_1_clicked();
+
+    void on_card3_2_clicked();
+
+    void on_card3_3_clicked();
+
+    void on_card3_4_clicked();
+
+
+    void on_selectBlack_clicked();
+    void on_selectRed_clicked();
+    void on_selectGreen_clicked();
+    void on_selectBlue_clicked();
+    void on_selectWhite_clicked();
+
+    void on_toolButton_clicked();
+
+
+    void on_toolButton_triggered(QAction *arg1);
+
+private:
+    Ui::gameInterface *ui;
+    Dialog *dialog;
+    Dialog2 *dialog2;
+    Table* table;
+
+
+    //点击宝石之后调用函数
+    void AnyDiamondClicked(enum color);
+    //点击卡片后调用显示函数
+    void AnyCardClicked(int level,int num);
+
+    //显示玩家拥有宝石
+    void on_white_have_textChanged();
+    void on_blue_have_textChanged();
+    void on_green_have_textChanged();
+    void on_red_have_textChanged();
+    void on_black_have_textChanged();
+    void on_gold_have_textChanged();
+    //显示场上宝石
+    void on_white_num_textChanged();
+    void on_black_num_textChanged();
+    void on_red_num_textChanged();
+    void on_blue_num_textChanged();
+    void on_green_num_textChanged();
+    void on_gold_num_textChanged();
+    //显示信息函数
+    void textChanged();//刷新信息，每次操作结束后调用
+    void textInit();//信息设置初始化，在构造函数中使用
+
+protected slots:
+   void DialogPurchaseSlot();
+   void DialogCancelSlot();
+   void dialog2OKSlot();
+   void dialog2CancelSlot();
+   void dialog2CancelTake(enum color);
+};
+
+#endif // GAMEINTERFACE_H
---
+++ 接口最新版本(合并前端后端)/interface/splendor.pro
@@ -0,0 +1,52 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2021-04-06T21:27:02
+#
+#-------------------------------------------------
+QT       += core gui
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = splendor
+TEMPLATE = app
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which as been marked as deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+
+SOURCES += \
+    Card.cpp \
+    Noble.cpp \
+    Player.cpp \
+    Table1.cpp \
+        main.cpp \
+        gameinterface.cpp \
+    dialog.cpp \
+    dialog2.cpp \
+    mainsurface.cpp \
+    rule.cpp
+
+HEADERS += \
+    Splendor.h \
+        gameinterface.h \
+    dialog.h \
+    dialog2.h \
+    mainsurface.h \
+    rule.h
+
+FORMS += \
+        gameinterface.ui \
+    dialog.ui \
+    dialog2.ui \
+    mainsurface.ui \
+    rule.ui
+
+RESOURCES += \
+    pictures.qrc
