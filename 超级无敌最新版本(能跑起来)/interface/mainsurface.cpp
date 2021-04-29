74abaaece3ff7f9744170cd8a611bf49c7e5d076
WinghoLeung <82169779+WinghoLeung@users.noreply.github.com>
2021-04-29 01:49:43 -0500

    新版本，修复析构函数没调用的大问题
    旧的超级无敌版本存在 析构函数没调用的内存泄漏问题，现在的interface文件夹是新版本
    并且游戏窗口也内嵌在主窗口内。

---
+++ 超级无敌最新版本(能跑起来)/interface/mainsurface.cpp
@@ -0,0 +1,34 @@
+#include "mainsurface.h"
+#include "ui_mainsurface.h"
+#include <QDebug>
+
+mainSurface::mainSurface(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::mainSurface)
+{
+    ui->setupUi(this);
+    gameSurface = nullptr;
+    r = nullptr;
+}
+
+mainSurface::~mainSurface()
+{
+    //qDebug()<<"MainSurface deleted."<<endl;
+    if(gameSurface != nullptr)
+        delete gameSurface;
+    if(r != nullptr)
+        delete r;
+    delete ui;
+}
+
+void mainSurface::on_start_button_clicked()
+{
+    gameSurface = new gameInterface(this);
+    gameSurface->show();
+}
+
+void mainSurface::on_rule_button_clicked()
+{
+    r = new Rule;
+    r->show();
+}
